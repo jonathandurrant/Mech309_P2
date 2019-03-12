ed7dafa07a5912087a89b2c254bc488aa49372a4
GregCarlesimo <48205344+GregCarlesimo@users.noreply.github.com>
2019-03-12 16:13:23 +0100

    Add files via upload
    secantE function necessary from MP1

---
+++ secantE.m
@@ -0,0 +1,30 @@
+% GREG
+
+function root = secantE (M, e);
+%% Solving for root E using Secant Method
+
+% Function definition 
+f = inline('x - a*sin(x) - b','x', 'a', 'b');
+
+% Initial guesses (in rad): 0 and 90 degrees 
+x(1)= 0;
+x(2)= 1.57;
+
+%% Start Iterations
+tol = 10^(-8);
+iteration = 0;
+max_iter = 100;
+
+for lv2=3:max_iter
+   x(lv2) = x(lv2-1) - (f(x(lv2-1), e, M))*((x(lv2-1) - x(lv2-2))/(f(x(lv2-1), e, M) - f(x(lv2-2), e, M)));
+    iteration=iteration+1;
+    if abs((x(lv2)-x(lv2-1))/x(lv2))*100<tol
+        root = x(lv2);
+        iteration = iteration;
+        break
+    end
+    
+% Return
+
+end
+
